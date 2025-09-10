6a42bfc6a57d29afdbf3ec742907122fce8d6a8e
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-09-10 00:15:08 -0700

    Add DojoClient module for dojo loading animation

--- StarterPlayer/StarterPlayerScripts/Boot.client.lua
+++ StarterPlayer/StarterPlayerScripts/Boot.client.lua
@@ -5,6 +5,9 @@ local BootModules = ReplicatedStorage:WaitForChild("BootModules")
 local PersonaUI = require(BootModules:WaitForChild("PersonaUI"))
 PersonaUI.start()
 
+local DojoClient = require(BootModules:WaitForChild("DojoClient"))
+DojoClient.start()
+
 local BootUI = require(BootModules:WaitForChild("BootUI"))
 local config = BootUI.fetchData()
 BootUI.start(config)
