2d8a4c446ccc4709130204a21e583def2cad5736
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-09-10 23:38:21 -0700

    Add purchasable item lists and server handling

---
+++ ReplicatedStorage/BootModules/ShopItems.lua
@@ -0,0 +1,12 @@
+local ShopItems = {
+    Elements = {
+        Fire = {cost = 100},
+        Beast = {cost = 150}
+    },
+    Weapons = {
+        ["Sword A"] = {cost = 200, assetId = 16232452668},
+        ["Bow A"] = {cost = 150, assetId = 16117888680}
+    }
+}
+
+return ShopItems
