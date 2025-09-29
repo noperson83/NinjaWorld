89442c0c860a5e3173d6741c13a5e7cf4623d592
Marcus A Lovato <30743921+noperson83@users.noreply.github.com>
2025-09-28 21:59:04 -0700

    Add new shop items

--- ReplicatedStorage/BootModules/ShopItems.lua
+++ ReplicatedStorage/BootModules/ShopItems.lua
@@ -5,7 +5,21 @@ local ShopItems = {
     },
     Weapons = {
         ["Sword A"] = {cost = 200, assetId = 16232452668},
-        ["Bow A"] = {cost = 150, assetId = 16117888680}
+        ["Sword B"] = {cost = 220, assetId = 16232532667},
+        ["Sword C"] = {cost = 240, assetId = 16232504981},
+        ["Sword D"] = {cost = 260, assetId = 16232534668},
+        ["Bow A"] = {cost = 150, assetId = 16117888680},
+        ["Bow B"] = {cost = 170, assetId = 16117890021},
+        ["Bow C"] = {cost = 190, assetId = 16117894011},
+        ["Bow D"] = {cost = 210, assetId = 16117895377},
+        ["BowBow"] = {cost = 230, assetId = 16232118442},
+        ["MetalMagic"] = {cost = 250, assetId = 125630227934002},
+        ["Longsleeve"] = {cost = 120, assetId = 125593084537583},
+        ["Shirt"] = {cost = 120, assetId = 125630227934002},
+        ["LMShirt"] = {cost = 110, assetId = 15899214466},
+        ["LMPants"] = {cost = 110, assetId = 15899822232},
+        ["Song 1"] = {cost = 80, assetId = 15933971668},
+        ["IceCoin"] = {cost = 140, assetId = 17799298968}
     }
 }
 
